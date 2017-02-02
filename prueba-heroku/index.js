58152a5f4caf9a337b0bb4b74fa97ea7285b6d7c
Ana Salas Urbano <anasalurb>
2017-02-02 19:48:04 +0100

    pets 3

---
+++ prueba-heroku/index.js
@@ -0,0 +1,15 @@
+console.log("Hello world!");
+var express = require("express"),
+    app = express();
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+   res.send("Hello World!");
+});
+
+app.use(router);
+
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
