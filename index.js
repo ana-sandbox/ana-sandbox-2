e8b9655e191e0390adc7e1521c6d5c2c08fcb3e9
Ana Salas Urbano <anasalurb>
2017-02-03 15:57:09 +0100

    pa heroku5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var serverPort = process.env.PORT;
+var serverPort = process.env.PORT || 8080;
 
 // swaggerRouter configuration
 var options = {
